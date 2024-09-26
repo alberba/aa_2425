e4eeef40583d82a6c297b4243c65b3f7c03fa8f4
alberba <83814585+alberba@users.noreply.github.com>
2024-09-26 17:45:50 +0200

    Day 2

--- 02_Non_linear_SVM/main.py
+++ 02_Non_linear_SVM/main.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score
 from sklearn.svm import SVC
+from sklearn.inspection import DecisionBoundaryDisplay
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import distance_matrix
 from sklearn.preprocessing import MinMaxScaler
@@ -21,7 +23,36 @@ scaler = MinMaxScaler() #StandardScaler()
 X_transformed = scaler.fit_transform(X_train)
 X_test_transformed = scaler.transform(X_test)
 
+# Divide por el número de características y la varianza
+gamma = 1.0/ (X_transformed.shape[1] * X_transformed.var())
 
 def kernel_lineal(x1, x2):
      return x1.dot(x2.T)
+
+def kernel_gauss(x1, x2):
+     return np.exp(-gamma * (distance_matrix(x1, x2))**2)
+
+def kernel_poly(x1, x2, degree=3):
+     return (gamma * x1.dot(x2.T)) ** degree
+
+kernels = {"linear": kernel_lineal, "rbf": kernel_gauss, "poly": kernel_poly}
+
+for kernel in kernels.keys() :
+     # Entrenamos con el kernel creado
+     svm_user = SVC(C=1000, kernel=kernels[kernel])
+     svm_user.fit(X_transformed, y_train, sample_weight=None)
+     y_predict_user = svm_user.predict(X_test)
+     print("Precision score", kernels[kernel].__name__ + ":", precision_score(y_true=y_test, y_pred=y_predict_user))
+     disp = DecisionBoundaryDisplay.from_estimator(svm_user, X_transformed, response_method="predict")
+     disp.ax_.scatter(X_transformed[:, 0], X_transformed[:, 1], edgecolor="k")
+     plt.show()
+
+     # Entrenamos con la función de la libreria
+     svm_lib = SVC(C=1000, kernel=kernel)
+     svm_lib.fit(X_transformed, y_train, sample_weight=None)
+     y_predict_lib = svm_lib.predict(X_test)
+     print("Precision score", kernel + ":", precision_score(y_true=y_test, y_pred=y_predict_lib))
+     print("")
+
+
 
